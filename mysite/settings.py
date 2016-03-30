edb5acc742227709e3cb6d7d207ddc18246a2fa6
zuga <zuga89@gmail.com>
2016-03-30 18:58:24 +0200

    Edited settings, missing ',' .

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangogirls',
-        'USER': 'marko'
+        'USER': 'marko',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
